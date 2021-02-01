a80b9d67f6fbd47f564b5ca2b4159b13a0dfb8bd
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2021-02-01 11:58:26 +0600

    adding number game

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 var numberbtn = document.querySelector('.numberbtn')
 var restartbtn = document.querySelector('.restartbtn')
 
-numberbtn.addEventListener('click',function(){
-    var myNumber = Math.floor((Math.random()*10+1))
-    for(let i=0;i<3;i++){
-        var value = prompt(`take a number between 1 to 10 : left chances ${3-(i+1)}`)
+numberbtn.addEventListener('click', function () {
+    var myNumber = Math.floor((Math.random() * 10 + 1))
+    for (let i = 0; i < 3; i++) {
+        var value = prompt(`take a number between 1 to 10 : left chances ${3 - (i + 1)}`)
         value = parseInt(value)
-        if(value === myNumber){
+        if (value === myNumber) {
             alert('You Win')
+            numberbtn.setAttribute("disabled", "")
+            restartbtn.setAttribute('style', 'display:inline');
             return
         }
-        else if(value>=myNumber){
+        else if (value >= myNumber) {
             alert('Correct answer is smaller!')
         }
-        else if(value<=myNumber){
+        else if (value <= myNumber) {
             alert('Correct answer is greater!')
         }
     }
     alert('You lose!')
-    numberbtn.setAttribute("disabled","")
+    numberbtn.setAttribute("disabled", "")
     restartbtn.setAttribute('style', 'display:inline');
 })
-restartbtn.addEventListener("click",function(){
+restartbtn.addEventListener("click", function () {
     numberbtn.disabled = false
     restartbtn.setAttribute('style', 'display:none');
 
