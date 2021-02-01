89f1269964e2e038a1a7083851461bc5c112eab3
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2021-02-01 11:56:25 +0600

    adding number game

---
+++ main.js
@@ -0,0 +1,28 @@
+var numberbtn = document.querySelector('.numberbtn')
+var restartbtn = document.querySelector('.restartbtn')
+
+numberbtn.addEventListener('click',function(){
+    var myNumber = Math.floor((Math.random()*10+1))
+    for(let i=0;i<3;i++){
+        var value = prompt(`take a number between 1 to 10 : left chances ${3-(i+1)}`)
+        value = parseInt(value)
+        if(value === myNumber){
+            alert('You Win')
+            return
+        }
+        else if(value>=myNumber){
+            alert('Correct answer is smaller!')
+        }
+        else if(value<=myNumber){
+            alert('Correct answer is greater!')
+        }
+    }
+    alert('You lose!')
+    numberbtn.setAttribute("disabled","")
+    restartbtn.setAttribute('style', 'display:inline');
+})
+restartbtn.addEventListener("click",function(){
+    numberbtn.disabled = false
+    restartbtn.setAttribute('style', 'display:none');
+
+})
